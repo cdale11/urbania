87bb4e122dff01f58af69f3d5dd961f1c85adbd5
cdale11 <clarkdale123@yahoo.com>
2026-09-01 17:25:24 +0530

    Add header to App and layout fixes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,8 +3,11 @@ import ThreeScene from './ThreeScene';
 
 const App: React.FC = () => {
   return (
-    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
-      <ThreeScene />
+    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0, display: 'flex', flexDirection: 'column' }}>
+      <h1 style={{ textAlign: 'center', margin: '0.5rem' }}>Urbania</h1>
+      <div style={{ flexGrow: 1 }}>
+        <ThreeScene />
+      </div>
     </div>
   );
 };
